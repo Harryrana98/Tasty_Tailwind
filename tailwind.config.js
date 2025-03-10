248aede89dc260d45752e1dee40ac9d9a162b969
Harryrana98 <ranaharendra77@gmail.com>
2025-03-10 11:17:11 +0530

    project complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'rotate-move': {
+          '0%': { transform: 'translateX(0) rotate(0deg)' },
+          '50%': { transform: 'translateX(50px) rotate(180deg)' },
+          '100%': { transform: 'translateX(0) rotate(360deg)' },
+        },
+      },
+      animation: {
+        'rotate-move': 'rotate-move 3s linear infinite',
+      },
+    },
   },
   plugins: [],
 }
